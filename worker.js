9b1f91c34b292578fba8211763ff0ccc5ae3bbfe
Roger <r1ger_pg@hotmail.com>
2017-08-24 20:32:44 +1000

    Updated Worker.JS file

--- worker.js
+++ worker.js
@@ -1,22 +1,8 @@
 
-var first = document.querySelector("#first");
-var result = document.querySelector("#result");
 
-// check if worker compatibility 
-if (window.Worker) {
-
-// confirm JS file on Github
-var newWorker = new Worker("worker.js");
-
-	first.onclick = function(el) {
-newWorker.postMessage(el.target.innerHTML);
-console.log('Message posted to worker');
-	};
-
-	newWorker.onmessage = function(e) {
-  result.innerHTML=e.target.innerHTML
-	console.log('Message received from worker');
-	};
-
-
-} // end of if worker
+onmessage = function(e) {
+  console.log('Message received from main script');
+  var workerResult = "Your name is"+e;
+  console.log("Message sent back");
+  postMessage(workerResult);
+}
