5488ce092eb1a18c58be755b9f0cdba88ee591c6
Roger <r1ger_pg@hotmail.com>
2017-08-25 13:33:39 +1000

    changes

--- worker.js
+++ worker.js
@@ -3,8 +3,8 @@
 onmessage = function(e) {
   console.log('Message received from main script');
   var workerResult = "Your name is"+e;
-  console.log("e: "+e);
-  console.log("workerResult: "+workerResult);
+  console.log("e: "+e.data);
+  console.log("workerResult: "+workerResult.data);
   console.log("Message sent back");
   postMessage(workerResult);
 }
