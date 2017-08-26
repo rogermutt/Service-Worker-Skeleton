acb19100da7f1857b0da2809b1619b4fcd900246
Roger <r1ger_pg@hotmail.com>
2017-08-27 09:49:25 +1000

    a

--- worker.js
+++ worker.js
@@ -2,11 +2,12 @@
 
 onmessage = function(e) {
 	console.log('Message received from main script');
+	console.log(e.data);	
  	var date = new Date (),
-	 	currently = 1900+date.getYear(),
+	 	currently = 1900 + date.getYear(),
   	  	workerResult = "You were born in "+ currently - e.data;
 
-  console.log("workerResult "+workerResult);
+
   console.log("Message sent back");	
 
   postMessage(workerResult);
