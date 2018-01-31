0460fef4f7352a5021a4c32c1f8c574a06691b38
Roger <r1ger_pg@hotmail.com>
2018-01-31 19:55:37 +1100

    Updated html and JS for new video

--- main.js
+++ main.js
@@ -1,37 +1,6 @@
 
 
-var input = document.querySelector("#input"),
-	button = document.querySelector("#submit"),
-	result = document.querySelector("#result");
 
-var newWorker = new Worker("worker.js");
-
-	button.onclick = function() { 
-  if (!isNaN(input.value)) {
-  newWorker.postMessage(input.value);
-	console.log('Message posted to worker');
-  } else {	alert("Digits only"); }
-	
-	};
-
-	newWorker.onmessage = function(e) {
-	console.log(e);
-  result.innerHTML=e.data;
-	console.log('Message received from worker');
-	};
-
-var myImage = document.querySelector('img');
-
-fetch('euripidies-image.jpg').then(function(response) {
-  return response.blob();
-}).then(function(myBlob) {
-  var objectURL = URL.createObjectURL(myBlob);
-  myImage.src = objectURL;
-
-    yourImg.style.height = '100px';
-    yourImg.style.width = '200px';
-  
-});
 
 
 
