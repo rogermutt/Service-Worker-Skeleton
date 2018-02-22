68c7a0f2f81e798c2b0c958ba975e84fc1e9f215
Roger <r1ger_pg@hotmail.com>
2018-02-22 17:11:37 +1100

    new version

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('CLIENT: service worker registration complete.');
-  }, function() {
-    console.log('CLIENT: service worker registration failure.');
+  navigator.serviceWorker.register('/sw.js', {
+    scope: '/'
   });
-} else {
-  console.log('CLIENT: service worker is not supported.');
 }
