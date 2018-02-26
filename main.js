49b3b36834e44abfe42d99738cd65385c156ada3
Roger <r1ger_pg@hotmail.com>
2018-02-26 14:46:51 +1100

    test

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(function(reg) {
-    // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
-}
+let proxyurl = "https://upload.wikimedia.org/wikipedia/commons/9/9c/Image-Porkeri_001.jpg";
+
+let myImage = new Image();
+
+let apiRequest3 =(url) => fetch(url)
+.then(response=>response.blob())
+.then(myBlob=>URL.createObjectURL(myBlob))
+.catch(function(e) { console.log("Req3 err: ", e); });
+
+apiRequest3(proxyurl).then(function(response) {
+console.log(response);
+  var imageURL = window.URL.createObjectURL(response);
+  myImage.src = imageURL;
+  body.appendChild(myImage);
+}, function(Error) {
+  console.log(Error);
+});
